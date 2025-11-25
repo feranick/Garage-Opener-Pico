0b21a45c39f49544c80e0116e9540de1d44598a3
Nicola Ferralis <feranick@hotmail.com>
2025-11-25 16:48:12 -0500

    Reorganized Outdoor UI

--- src/remote/code.py
+++ src/remote/code.py
@@ -1,10 +1,10 @@
 # **********************************************
 # * Garage Opener - Rasperry Pico W
-# * v2025.11.24.2
+# * v2025.11.25.1
 # * By: Nicola Ferralis <feranick@hotmail.com>
 # **********************************************
 
-version = "2025.11.24.2"
+version = "2025.11.25.1"
 
 import wifi
 import time
